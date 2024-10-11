f9083efc1214424f2c811380ec70774450a7a6fc
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-10-11 09:13:35 -0300

    Update controllers.py

--- saturn/controllers/controllers.py
+++ saturn/controllers/controllers.py
@@ -1,13 +1,32 @@
-from flask import Blueprint, render_template
-from models.models import p, pessoa
+from flask import Blueprint, render_template, session, redirect, url_for, request
+from models.models import Users
 
-exemplo = Blueprint("exemplo", __name__)
+projeto = Blueprint("exemplo", __name__)
 
-@exemplo.route("/")
-def exibir():
-    return render_template('index.html')
+@projeto.route("/")
+def index():
+    if 'username' in session:
+        return f'Bem-vindo, {session["username"]}!'
+    return 'Você não está logado'
 
-@exemplo.route("/oi")
-def exibir2():
-    return render_template('index.html', sla = p, nsei=pessoa)
-  
+@projeto.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method =='POST':
+        session['username']=request.form['username']
+        return redirect(url_for('index'))
+    return render_template("index.html")
+    
+@projeto.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+#projeto = Blueprint("exemplo", __name__)
+
+#@app.route("/")
+#def index():
+    #return render_template('index.html')
+
+#@app.route("/oi")
+#def exibir2():
+    #return render_template('index.html', sla = p, nsei=pessoa)
