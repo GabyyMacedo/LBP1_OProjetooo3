a811772a46d3c3615f1e9dbc3b25efe3c069d0fc
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-11-01 09:43:18 -0300

    Delete saturn directory

--- saturn/models/Usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self,id,nome,senha):
-        self.id=id
-        self.nome=nome
-        self.senha=senha
-
-listaUsuarios=[]
-listaUsuarios.append(Usuario(1, "Gaby", "1234"))
-listaUsuarios.append(Usuario(2, "Gigi", "5678"))
