faebcd341d076a1ae7a0a30e38b9aa5e27cde978
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-11-01 09:43:25 -0300

    Delete erros.py

--- erros.py
+++
@@ -1,7 +0,0 @@
-@app.errorhandler(403)
-def page_not_found(e):
-    return render_template('403.html'), 403
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
